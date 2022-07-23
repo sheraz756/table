dde61a1c4dd583af14027fa8a81d3d7a039f1ba7
sheraz <sherazzafar.148@gmail.com>
2022-07-23 19:09:25 +0500

    second change

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function onclicking()
 {
     let a = +document.querySelector("#value").value;
     let b = a;
+    document.querySelector("#result").innerHTML = "";
     for (a=1;a<=10;a++){
         document.querySelector("#result").innerHTML+=b+"x"+a+"="+a*b+"<br>"
          }
